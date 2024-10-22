ae4740cdc59a5d656f21033e40cad230dc42aa9e
Branden Dunbar <branden.dunbar@pepperstone.com>
2024-10-22 14:50:51 +1100

    known ex.

---
+++ nosql_injection/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_pymongo import PyMongo
+import json
+
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def home_page():
+    unsanitized_search = request.args['search']
+    json_search = json.loads(unsanitized_search)
+
+    result = mongo.db.user.find({'name': json_search})
